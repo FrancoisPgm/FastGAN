95877b70003987d7a323d1a5058c14f9c560f0cc
François Paugam <francois.paugam@laposte.net>
2026-07-25 11:41:30 +0200

    actually add conf file

---
+++ FastGAN/conf.py
@@ -0,0 +1,8 @@
+GAN_CKPT = "/Users/fpaugam/Documents/code/capsule_fastgan/output/test_capsule/train_results/test_512_cpu_3/models/30000.pth"
+
+IM_PATH = "../../Images_eve_jpeg/IMG_5448.jpeg"
+
+SAM2_CKPT = "/Users/fpaugam/Documents/code/capsule_fastgan/sam2/checkpoints/sam2.1_hiera_small.pt"
+SAM2_CFG = "configs/sam2.1/sam2.1_hiera_s.yaml"
+DEVICE = "cpu"
+IM_SIZE = 512
